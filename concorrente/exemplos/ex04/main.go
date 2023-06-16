8b9764115ad465ba1116ab07524540fae00462e7
Nelson <nsr@cin.ufpe.br>
2023-06-16 16:47:56 -0300

    Pequenos ajustes - Mutex queue

--- concorrente/exemplos/ex04/main.go
+++ concorrente/exemplos/ex04/main.go
@@ -5,7 +5,7 @@ import (
 	"sync/atomic"
 )
 
-func add(w *sync.WaitGroup, num *int) {
+func add(w *sync.WaitGroup, num *int32) {
 	defer w.Done()
 	*num = *num + 1
 }
@@ -17,17 +17,15 @@ func addAtomic(w *sync.WaitGroup, num *int32) {
 
 func main() {
 	var n int32 = 0 // atomic
-	//var m int = 0
 
 	var wg = new(sync.WaitGroup)
 	wg.Add(1000)
 
 	for i := 0; i < 1000; i++ {
-		go addAtomic(wg, &n)  // atomic
-		//go add(wg, &m)
+		//go addAtomic(wg, &n) // atomic
+		go add(wg, &n)
 	}
 	wg.Wait()
 
-	println(n)  // atomic
-	//println(m)
+	println(n) // atomic
 }
--- concorrente/synccond/main/main.go
+++ concorrente/synccond/main/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"aulas/concorrente/synccond/mutexqueue"
 	"fmt"
-   "multithread/synccond/mutexqueue"
-   "sync"
+	"sync"
 )
 
 func main() {
 
-   var wg = new(sync.WaitGroup)
+	var wg = new(sync.WaitGroup)
 
-   //var q = condqueue.NewCondQueue(3)
-   var q = mutexqueue.NewMutexQueue(3)
+	//var q = condqueue.NewCondQueue(3)
+	var q = mutexqueue.NewMutexQueue(3)
 
-   wg.Add(5)
+	wg.Add(5)
 
-   go q.Push(1,wg)
-   go q.Push(2,wg)
-   go q.Push(3,wg)
-   go q.Push(4,wg)
-   go q.Pop(wg)
+	go q.Push(1, wg)
+	go q.Push(2, wg)
+	go q.Push(3, wg)
+	go q.Push(4, wg)
+	go q.Pop(wg)
 
-   fmt.Println(q)
+	fmt.Println(q)
 }
