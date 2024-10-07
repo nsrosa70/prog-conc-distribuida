526569003e69f388a22c032919018a6357abfa1e
Nelson <nsr@cin.ufpe.br>
2024-10-07 12:04:15 -0300

    Atualização do Cliente/Servidor do MyMiddleware.

--- mymiddleware/app/client/cliente.go
+++ mymiddleware/app/client/cliente.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"test/mymiddleware/distribution/proxies/calculadora"
-	"test/mymiddleware/distribution/proxies/fibonacci"
 	namingproxy "test/mymiddleware/services/naming/proxy"
 	"test/shared"
 )
@@ -17,11 +16,7 @@ func Cliente() {
 	// Obtain proxies
 	naming := namingproxy.New(shared.LocalHost, shared.NamingPort)
 	calc := calculadoraproxy.New(naming.Find("Calculadora"))
-	fibo := fibonacciproxy.New(shared.LocalHost, shared.FibonacciPort)
 
-	// Invoke services
-	for i := 0; i < 1000; i++ {
-		fmt.Println(i, calc.Som(i, i), calc.Dif(i, i), calc.Mul(i, i), calc.Div(i, i))
-		fmt.Println(i, fibo.Fibo(i))
-	}
+	// Chamada remota a Calculadora
+	fmt.Println(calc.Som(1, 2))
 }
